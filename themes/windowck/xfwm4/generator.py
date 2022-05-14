34efc9dbf538da52e9e42eef296762a0aa6b6318
Arkadiy Illarionov <qarkai@gmail.com>
2022-05-14 18:47:12 +0300

    Deduplicate xfwm4 icons creation

--- themes/windowck/xfwm4/generator.py
+++ themes/windowck/xfwm4/generator.py
@@ -20,111 +20,13 @@
 
 '''
 
-from os import linesep
+import sys
+from pathlib import Path
 
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
 
-def generate(name, txt, dic, x0=0, y0=0, w=None, h=None):
-    ''' Creates xpm file with given name, given draw as string, colors as dict.
-        Extra args are for generate parts of xpm.
-    '''
-    if w is None:
-        w = len(txt.split("\n")[0])
-    if h is None:
-        h = len(txt.split("\n"))
-    x1 = x0 + w
-    y1 = y0 + h
-    colors = {}
-    lines = [i[x0:x1] for i in txt.split("\n")[y0:y1]]
-    for i in lines:
-        for j in i:
-            if j not in colors:
-                colors[j] = dic[j]
-    xpmlines = [
-        "/* XPM */",
-        f'static char * {name.replace("-", "_")} = {{',
-        f'"{w} {h} {len(colors)} 1", '
-        ]
-    xpmlines.extend(
-        f'"{i[0]}\tc {i[1]}", ' for i in list(colors.items())
-        )
-    xpmlines.extend(
-        f'"{i}", ' for i in lines
-        )
-    xpmlines.append(
-        "};"
-        )
-    with open(f"{name}.xpm", "w") as f: f.write(linesep.join(xpmlines))
+from generator_xfwm4 import IconMap, build
 
-def holePos(txt):
-    ''' Detects a hole on a xpm string, used to find border sizes.'''
-    lines = txt.split("\n")
-    for i, line in enumerate(lines):
-        if " " in line:
-            return (line.find(" "), i)
-    raise ValueError
-
-def holeSize(txt):
-    ''' Detects hole on a xpm string, used to find border sizes.'''
-    lastwidth = 0
-    inhole = 0
-    for line in txt.split("\n"):
-        if " " in line:
-            lastwidth = line.count(" ")
-            inhole += 1
-        elif inhole > 0:
-            return (lastwidth, inhole)
-    raise ValueError
-
-def build():
-    gvar = globals()
-    for i in ("close", "hide", "maximize", "maximize_toggled", "menu", "shade", "shade_toggled", "stick", "stick_toggled"):
-        for j in ("active", "pressed", "inactive", "prelight"):
-            name = f"{i}_{j}"
-            if name in gvar:
-                generate(name.replace("_", "-"), gvar[name], gvar[f"{name}_map"])
-    
-    alw, alh = (len(active.split("\n")[0]),len(active.split("\n")))
-    ilw, ilh = (len(inactive.split("\n")[0]),len(inactive.split("\n")))
-
-    ''' Find corner length on a xpm string.'''
-    acw= len(active.split("+")[0])
-    icw= len(inactive.split("+")[0])
-
-    ahx, ahy = holePos(active)
-    ihx, ihy = holePos(inactive)
-    ahw, ahh = holeSize(active)
-    ihw, ihh = holeSize(inactive)
-    abw, abh = (alw-ahx-ahw, alh-ahy-ahh)
-    ibw, ibh = (ilw-ihx-ihw, ilh-ihy-ihh)
-    
-    #top-left
-    generate("top-left-active", active, amap, 0, 0, acw, ahy)
-    generate("top-left-inactive", inactive, imap, 0, 0, icw, ihy)
-    #left
-    generate("left-active", active, amap, 0, ahy, ahx, ahh)
-    generate("left-inactive", inactive, imap, 0, ihy, ihx, ihh)
-    #bottom-left
-    generate("bottom-left-active", active, amap, 0, ahy+ahh, ahx, abh)
-    generate("bottom-left-inactive", inactive, imap, 0, ihy+ihh, ihx, ibh)
-
-    #top-right
-    generate("top-right-active", active, amap, alw-acw, 0, acw, ahy)
-    generate("top-right-inactive", inactive, imap, ilw-icw, 0, icw, ihy)
-    #right
-    generate("right-active", active, amap, ahx+ahw, ahy, abw, ahh)
-    generate("right-inactive", inactive, imap, ihx+ihw, ihy,  ibw, ihh)
-    #bottom-right
-    generate("bottom-right-active", active, amap, ahx+ahw, ahy+ahh, abw, abh)
-    generate("bottom-right-inactive", inactive, imap, ihx+ihw, ihy+ihh, ibw, ibh)
-
-    #top
-    for i in range(1,6):
-        generate(f"title-{i}-active", active, amap, acw, 0, alw-2*acw, ahy)
-        generate(f"title-{i}-inactive", inactive, imap, icw, 0, alw-2*icw, ihy)
-
-    #bottom
-    generate("bottom-active", active, amap, ahx, ahy+ahh, ahw, ibh)
-    generate("bottom-inactive", inactive, imap, ihx, ihy+ihh, ihw, ibh)
 
 #close
 close_active = '''
@@ -144,8 +46,6 @@ close_active = '''
 ==================
 '''.strip()
 
-chars = ["+", "@", "#", "=", "-"]
-dmap = [(".", "None")]
 close_active_map = {
     "." : "None",
     "=" : "#000000 s  active_color_2",
@@ -154,7 +54,6 @@ close_active_map = {
     "#" : "None",
 }
 
-close_prelight = close_active
 close_prelight_map = {
     "." : "None",
     "=" : "#000000 s  active_color_2",
@@ -163,16 +62,6 @@ close_prelight_map = {
     "#" : "None",
 }
 
-close_pressed = close_active
-close_pressed_map = {
-    "." : "None",
-    "=" : "#000000 s  active_color_2",
-    "+" : "#D92626",
-    "@" : "#000000 s  active_color_2",
-    "#" : "None",
-}
-
-close_inactive = close_active
 close_inactive_map = {
     "." : "None",
     "=" : "#000000 s inactive_color_2",
@@ -198,8 +87,7 @@ hide_active = '''
 ===@@@@@@@@@@@@===
 ==================
 '''.strip()
-hide_active_map = close_active_map
-hide_prelight = hide_active
+
 hide_prelight_map = {
     "." : "None",
     "=" : "#000000 s  active_color_2",
@@ -207,16 +95,12 @@ hide_prelight_map = {
     "@" : "#FFFFFF",
 }
 
-hide_pressed = hide_active
 hide_pressed_map = {
     "." : "None",
     "=" : "#000000 s  active_color_2",
     "+" : "#000000",
     "@" : "#000000 s  active_color_2",
 }
-
-hide_inactive = hide_active
-hide_inactive_map = close_inactive_map
 
 #maximize
 maximize_active = '''
@@ -235,13 +119,6 @@ maximize_active = '''
 ===@@@@@@@@@@@@===
 ==================
 '''.strip()
-maximize_active_map = close_active_map
-maximize_prelight = maximize_active
-maximize_prelight_map = hide_prelight_map
-maximize_pressed = maximize_active
-maximize_pressed_map = hide_pressed_map
-maximize_inactive = maximize_active
-maximize_inactive_map = close_inactive_map
 
 #maximize-toggled
 maximize_toggled_active = '''
@@ -260,13 +137,6 @@ maximize_toggled_active = '''
 ====@@@@@@@@@@====
 ==================
 '''.strip()
-maximize_toggled_active_map = close_active_map
-maximize_toggled_prelight = maximize_toggled_active
-maximize_toggled_prelight_map = hide_prelight_map
-maximize_toggled_pressed = maximize_toggled_active
-maximize_toggled_pressed_map = hide_pressed_map
-maximize_toggled_inactive = maximize_toggled_active
-maximize_toggled_inactive_map = close_inactive_map
 
 #menu
 menu_active = '''
@@ -285,13 +155,6 @@ menu_active = '''
 ==================
 ==================
 '''.strip()
-menu_active_map = close_active_map
-menu_prelight = menu_active
-menu_prelight_map = hide_prelight_map
-menu_pressed = menu_active
-menu_pressed_map = hide_prelight_map
-menu_inactive = menu_active
-menu_inactive_map = close_inactive_map
 
 #shade
 shade_active = '''
@@ -310,13 +173,6 @@ shade_active = '''
 ========@@========
 ==================
 '''.strip()
-shade_active_map = close_active_map
-shade_prelight = shade_active
-shade_prelight_map = hide_prelight_map
-shade_pressed = shade_active
-shade_pressed_map = hide_pressed_map
-shade_inactive = shade_active
-shade_inactive_map = menu_inactive_map
 
 #shade-toggled
 shade_toggled_active = '''
@@ -335,13 +191,6 @@ shade_toggled_active = '''
 ========@@========
 ==================
 '''.strip()
-shade_toggled_active_map = close_active_map
-shade_toggled_prelight = shade_toggled_active
-shade_toggled_prelight_map = hide_prelight_map
-shade_toggled_pressed = shade_toggled_active
-shade_toggled_pressed_map = hide_pressed_map
-shade_toggled_inactive = shade_toggled_active
-shade_toggled_inactive_map = menu_inactive_map
 
 #stick
 stick_active = '''
@@ -360,13 +209,6 @@ stick_active = '''
 =======@@@@=======
 ==================
 '''.strip()
-stick_active_map = close_active_map
-stick_prelight = stick_active
-stick_prelight_map = hide_prelight_map
-stick_pressed = stick_active
-stick_pressed_map = hide_pressed_map
-stick_inactive = stick_active
-stick_inactive_map = menu_inactive_map
 
 #stick-toggled
 stick_toggled_active = '''
@@ -385,13 +227,62 @@ stick_toggled_active = '''
 =======@@@@=======
 ==================
 '''.strip()
-stick_toggled_active_map = close_active_map
-stick_toggled_prelight = stick_toggled_active
-stick_toggled_prelight_map = hide_prelight_map
-stick_toggled_pressed = stick_toggled_active
-stick_toggled_pressed_map = hide_pressed_map
-stick_toggled_inactive = stick_toggled_active
-stick_toggled_inactive_map = menu_inactive_map
+
+icons = {
+    # close
+    'close-active': IconMap(close_active, close_active_map),
+    'close-prelight': IconMap(close_active, close_prelight_map),
+    'close-pressed': IconMap(close_active, close_prelight_map),
+    'close-inactive': IconMap(close_active, close_inactive_map),
+
+    # hide
+    'hide-active': IconMap(hide_active, close_active_map),
+    'hide-prelight': IconMap(hide_active, hide_prelight_map),
+    'hide-pressed': IconMap(hide_active, hide_pressed_map),
+    'hide-inactive': IconMap(hide_active, close_inactive_map),
+
+    # maximize
+    'maximize-active': IconMap(maximize_active, close_active_map),
+    'maximize-prelight': IconMap(maximize_active, hide_prelight_map),
+    'maximize-pressed': IconMap(maximize_active, hide_pressed_map),
+    'maximize-inactive': IconMap(maximize_active, close_inactive_map),
+
+    # maximize-toggled
+    'maximize-toggled-active': IconMap(maximize_toggled_active, close_active_map),
+    'maximize-toggled-prelight': IconMap(maximize_toggled_active, hide_prelight_map),
+    'maximize-toggled-pressed': IconMap(maximize_toggled_active, hide_pressed_map),
+    'maximize-toggled-inactive': IconMap(maximize_toggled_active, close_inactive_map),
+
+    # menu
+    'menu-active': IconMap(menu_active, close_active_map),
+    'menu-prelight': IconMap(menu_active, hide_prelight_map),
+    'menu-pressed': IconMap(menu_active, hide_prelight_map),
+    'menu-inactive': IconMap(menu_active, close_inactive_map),
+
+    # shade
+    'shade-active': IconMap(shade_active, close_active_map),
+    'shade-prelight': IconMap(shade_active, hide_prelight_map),
+    'shade-pressed': IconMap(shade_active, hide_pressed_map),
+    'shade-inactive': IconMap(shade_active, close_inactive_map),  # menu_inactive_map
+
+    # shade-toggled
+    'shade-toggled-active': IconMap(shade_toggled_active, close_active_map),
+    'shade-toggled-prelight': IconMap(shade_toggled_active, hide_prelight_map),
+    'shade-toggled-pressed': IconMap(shade_toggled_active, hide_pressed_map),
+    'shade-toggled-inactive': IconMap(shade_toggled_active, close_inactive_map),  # menu_inactive_map
+
+    # stick
+    'stick-active': IconMap(stick_active, close_active_map),
+    'stick-prelight': IconMap(stick_active, hide_prelight_map),
+    'stick-pressed': IconMap(stick_active, hide_pressed_map),
+    'stick-inactive': IconMap(stick_active, close_inactive_map),  # menu_inactive_map
+
+    # stick-toggled
+    'stick-toggled-active': IconMap(stick_toggled_active, close_active_map),
+    'stick-toggled-prelight': IconMap(stick_toggled_active, hide_prelight_map),
+    'stick-toggled-pressed': IconMap(stick_toggled_active, hide_pressed_map),
+    'stick-toggled-inactive': IconMap(stick_toggled_active, close_inactive_map),  # menu_inactive_map
+}
 
 active = '''
 ..+++++++++++..
@@ -451,4 +342,4 @@ imap = {
 }
 
 if __name__ == "__main__":
-    build()
+    build(icons, IconMap(active, amap), IconMap(inactive, imap))
