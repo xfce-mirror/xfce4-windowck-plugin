6ee3de73c29a0e276870c7cf820f58214115288f
Arkadiy Illarionov <qarkai@gmail.com>
2022-05-15 15:00:11 +0000

    Modernize generator.py scripts
    * use f-strings
    * use `subprocess` and `shutil` instead of `os.system`
    * use `enumerate` instead of `range(len)`
    * remove unused functions
    * deduplicate code

---
+++ themes/generator_common.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+"""
+simple-gtk xpm generator
+
+Copyright (C) 2012  Felipe A. Hernandez <spayder26@gmail.com>
+Portions adapted by Cedric Leporcq.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import shutil
+import subprocess
+from dataclasses import dataclass
+from os import linesep
+
+
+@dataclass
+class IconMap:
+    data: str
+    colors: dict
+
+
+def generate(name: str, icon: IconMap, x0=0, y0=0, w=None, h=None):
+    """ Creates xpm file with given name, given draw as string, colors as dict.
+        Extra args are for generate parts of xpm.
+    """
+    icon_lines = icon.data.split("\n")
+    if w is None:
+        w = len(icon_lines[0])
+    if h is None:
+        h = len(icon_lines)
+    x1 = x0 + w
+    y1 = y0 + h
+    colors = {}
+    lines = [line[x0:x1] for line in icon_lines[y0:y1]]
+    for line in lines:
+        for c in line:
+            if c not in colors:
+                colors[c] = icon.colors[c]
+
+    xpmlines = [
+        "/* XPM */",
+        f'static char * {name.replace("-", "_")} = {{',
+        f'"{w} {h} {len(colors)} 1", '
+    ]
+    xpmlines.extend(f'"{i[0]}\tc {i[1]}", ' for i in list(colors.items()))
+    xpmlines.extend(f'"{i}", ' for i in lines)
+    xpmlines.append("};")
+
+    with open(f"{name}.xpm", "w") as f:
+        f.write(linesep.join(xpmlines))
+
+
+def hole_pos(txt):
+    """ Detects a hole on a xpm string, used to find border sizes."""
+    lines = txt.split("\n")
+    for i, line in enumerate(lines):
+        if " " in line:
+            return line.find(" "), i
+    raise ValueError
+
+
+def hole_size(txt):
+    """ Detects hole on a xpm string, used to find border sizes."""
+    lastwidth = 0
+    inhole = 0
+    for line in txt.split("\n"):
+        if " " in line:
+            lastwidth = line.count(" ")
+            inhole += 1
+        elif inhole > 0:
+            return lastwidth, inhole
+    raise ValueError
+
+
+def build_xfwm4(icons: dict, active: IconMap, inactive: IconMap):
+    for name, icon in icons.items():
+        generate(name, icon)
+
+    alines = active.data.split("\n")
+    ilines = inactive.data.split("\n")
+    alw, alh = (len(alines[0]), len(alines))
+    ilw, ilh = (len(ilines[0]), len(ilines))
+
+    ''' Find corner length on a xpm string.'''
+    acw = len(active.data.split("+")[0])
+    icw = len(inactive.data.split("+")[0])
+
+    ahx, ahy = hole_pos(active.data)
+    ihx, ihy = hole_pos(inactive.data)
+    ahw, ahh = hole_size(active.data)
+    ihw, ihh = hole_size(inactive.data)
+    abw, abh = (alw - ahx - ahw, alh - ahy - ahh)
+    ibw, ibh = (ilw - ihx - ihw, ilh - ihy - ihh)
+
+    # top-left
+    generate("top-left-active", active, 0, 0, acw, ahy)
+    generate("top-left-inactive", inactive, 0, 0, icw, ihy)
+    # left
+    generate("left-active", active, 0, ahy, ahx, ahh)
+    generate("left-inactive", inactive, 0, ihy, ihx, ihh)
+    # bottom-left
+    generate("bottom-left-active", active, 0, ahy + ahh, ahx, abh)
+    generate("bottom-left-inactive", inactive, 0, ihy + ihh, ihx, ibh)
+
+    # top-right
+    generate("top-right-active", active, alw - acw, 0, acw, ahy)
+    generate("top-right-inactive", inactive, ilw - icw, 0, icw, ihy)
+    # right
+    generate("right-active", active, ahx + ahw, ahy, abw, ahh)
+    generate("right-inactive", inactive, ihx + ihw, ihy, ibw, ihh)
+    # bottom-right
+    generate("bottom-right-active", active, ahx + ahw, ahy + ahh, abw, abh)
+    generate("bottom-right-inactive", inactive, ihx + ihw, ihy + ihh, ibw, ibh)
+
+    # top
+    for i in range(1, 6):
+        generate(f"title-{i}-active", active, acw, 0, alw - 2 * acw, ahy)
+        generate(f"title-{i}-inactive", inactive, icw, 0, alw - 2 * icw, ihy)
+
+    # bottom
+    generate("bottom-active", active, ahx, ahy + ahh, ahw, ibh)
+    generate("bottom-inactive", inactive, ihx, ihy + ihh, ihw, ibh)
+
+
+def build_unity(icons: dict):
+    for name, icon in icons.items():
+        generate(name, icon)
+        try:
+            subprocess.call(["convert", f"{name}.xpm", f"{name}.png"])
+        except FileNotFoundError as e:
+            print(f"Can't convert {name}.xpm to {name}.png: {e.strerror} '{e.filename}'" )
+
+    for i in ("close", "maximize", "minimize", "menu", "unmaximize"):
+        try:
+            shutil.copy2(f"{i}_focused_normal.png", f"{i}.png")
+        except FileNotFoundError as e:
+            print(f"Can't copy {i}_focused_normal.png to {i}.png: {e.strerror} '{e.filename}'" )
--- themes/windowck/unity/generator.py
+++ themes/windowck/unity/generator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''
     simple-gtk xpm generator
-    
+
     Copyright (C) 2012  Felipe A. Hernandez <spayder26@gmail.com>
     Portions adapted by Cedric Leporcq.
 
@@ -19,107 +19,14 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 '''
-import os
-from os import linesep
 
+import sys
+from pathlib import Path
 
-def gendeg2(color1, color2, steps):
-    ''' Generates a list of colors being a gradient from color1 to color2 on
-        given steps number. '''
-    def fix(c):
-        if c < 0: return 0
-        elif c > 255: return 255
-        return int(round(c))
-    a = [0]*steps
-    c1 = (int(color1[1:3],16),int(color1[3:5],16),int(color1[5:7],16))
-    c2 = (int(color2[1:3],16),int(color2[3:5],16),int(color2[5:7],16))
-    ci = (float(c2[0]-c1[0])/(steps+1),float(c2[1]-c1[1])/(steps+1),float(c2[2]-c1[2])/(steps+1))
-    tr = [c1]
-    for i in range(steps):
-        tr.append((fix(tr[-1][0]+ci[0]),fix(tr[-1][1]+ci[1]),fix(tr[-1][2]+ci[2])))
-    tr.append(c2)
-    return [("#%2s%2s%2s" % (hex(i[0])[2:],hex(i[1])[2:],hex(i[2])[2:])).replace(" ", "0") for i in tr]
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
 
-def gendeg3(color1, color2, color3, steps):
-    ''' Generates a list of colors being a gradient from color1 to color3, with
-        color2 at middle, with given steps number between each color. '''
-    return gendeg(color1,color2,steps)[:-1]+gendeg(color2,color3,steps)
+from generator_common import IconMap, build_unity
 
-def gendeg(*args):
-    ''' Interface between gendeg2 or gendeg3 depending on arg number '''
-    if len(args) == 3: return gendeg2(*args)
-    elif len(args) == 4: return gendeg3(*args)
-    raise NotImplemented("Bad arguments, see gendeg2 and gendeg3 documentation.")
-
-def genmap(dmap, chars, *gendeg_args):
-    ''' 
-
-    '''
-    r = dict(zip(chars,gendeg(*gendeg_args)))
-    r.update(dmap)
-    return r
-
-def generate(name, txt, dic, x0=0, y0=0, w=None, h=None):
-    ''' Creates xpm file with given name, given draw as string, colors as dict.
-        Extra args are for generate parts of xpm.
-    '''
-    if w is None:
-        w = len(txt.split("\n")[0])
-    if h is None:
-        h = len(txt.split("\n"))
-    x1 = x0 + w
-    y1 = y0 + h
-    colors = {}
-    lines = [i[x0:x1] for i in txt.split("\n")[y0:y1]]
-    for i in lines:
-        for j in i:
-            if j not in colors:
-                colors[j] = dic[j]
-    xpmlines = [
-        "/* XPM */",
-        "static char * %s = {" % name.replace("-", "_"),
-        "\"%d %d %d 1\", " % (w, h, len(colors))
-        ]
-    xpmlines.extend(
-        "\"%s\tc %s\", " % i for i in list(colors.items())
-        )
-    xpmlines.extend(
-        "\"%s\", " % i for i in lines
-        )
-    xpmlines.append(
-        "};"
-        )
-    with open("%s.xpm" % name,"w") as f: f.write(linesep.join(xpmlines))
-
-def holePos(txt):
-    ''' Detects a hole on a xpm string, used to find border sizes.'''
-    lines = txt.split("\n")
-    for i in range(len(lines)):
-        if " " in lines[i]:
-            return (lines[i].find(" "),i)
-    raise ValueError
-
-def holeSize(txt):
-    ''' Detects hole on a xpm string, used to find border sizes.'''
-    lastwidth = 0
-    inhole = 0
-    for line in txt.split("\n"):
-        if " " in line:
-            lastwidth = line.count(" ")
-            inhole += 1
-        elif inhole > 0:
-            return (lastwidth, inhole)
-    raise ValueError
-
-def build():
-    gvar = globals()
-    for i in ("close", "maximize", "minimize", "menu", "unmaximize"):
-        for j in ("focused_normal", "focused_prelight", "focused_pressed", "unfocused"):
-            name = "%s_%s" % (i,j)
-            if name in gvar:
-                generate(name, gvar[name], gvar["%s_map" % name])
-                os.system("convert %s.xpm %s.png" % (name,name))
-        os.system("cp %s_focused_normal.png %s.png" % (i,i))
 
 #close
 close_focused_normal = '''
@@ -139,8 +46,6 @@ close_focused_normal = '''
 ==================
 '''.strip()
 
-chars = ["+", "@", "#", "=", "-"]
-dmap = [(".", "None")]
 close_focused_normal_map = {
     "." : "None",
     "=" : "None",
@@ -149,7 +54,6 @@ close_focused_normal_map = {
     "#" : "None",
 }
 
-close_focused_prelight = close_focused_normal
 close_focused_prelight_map = {
     "." : "None",
     "=" : "None",
@@ -158,7 +62,6 @@ close_focused_prelight_map = {
     "#" : "None",
 }
 
-close_focused_pressed = close_focused_normal
 close_focused_pressed_map = {
     "." : "None",
     "=" : "None",
@@ -167,7 +70,6 @@ close_focused_pressed_map = {
     "#" : "None",
 }
 
-close_unfocused = close_focused_normal
 close_unfocused_map = {
     "." : "None",
     "=" : "None",
@@ -193,8 +95,7 @@ minimize_focused_normal = '''
 ===@@@@@@@@@@@@===
 ==================
 '''.strip()
-minimize_focused_normal_map = close_focused_normal_map
-minimize_focused_prelight = minimize_focused_normal
+
 minimize_focused_prelight_map = {
     "." : "None",
     "=" : "None",
@@ -203,7 +104,6 @@ minimize_focused_prelight_map = {
     "#" : "#000000",
 }
 
-minimize_focused_pressed = minimize_focused_normal
 minimize_focused_pressed_map = {
     "." : "None",
     "=" : "None",
@@ -211,9 +111,6 @@ minimize_focused_pressed_map = {
     "@" : "None",
     "#" : "#000000",
 }
-
-minimize_unfocused = minimize_focused_normal
-minimize_unfocused_map = close_unfocused_map
 
 #maximize
 maximize_focused_normal = '''
@@ -232,13 +129,6 @@ maximize_focused_normal = '''
 ===@@@@@@@@@@@@===
 ==================
 '''.strip()
-maximize_focused_normal_map = close_focused_normal_map
-maximize_focused_prelight = maximize_focused_normal
-maximize_focused_prelight_map = minimize_focused_prelight_map
-maximize_focused_pressed = maximize_focused_normal
-maximize_focused_pressed_map = minimize_focused_pressed_map
-maximize_unfocused = maximize_focused_normal
-maximize_unfocused_map = close_unfocused_map
 
 #maximize-toggled
 unmaximize_focused_normal = '''
@@ -257,13 +147,6 @@ unmaximize_focused_normal = '''
 ====@@@@@@@@@@====
 ==================
 '''.strip()
-unmaximize_focused_normal_map = close_focused_normal_map
-unmaximize_focused_prelight = unmaximize_focused_normal
-unmaximize_focused_prelight_map = minimize_focused_prelight_map
-unmaximize_focused_pressed = unmaximize_focused_normal
-unmaximize_focused_pressed_map = minimize_focused_pressed_map
-unmaximize_unfocused = unmaximize_focused_normal
-unmaximize_unfocused_map = close_unfocused_map
 
 #menu
 menu_focused_normal = '''
@@ -282,13 +165,38 @@ menu_focused_normal = '''
 ==================
 ==================
 '''.strip()
-menu_focused_normal_map = close_focused_normal_map
-menu_focused_prelight = menu_focused_normal
-menu_focused_prelight_map = minimize_focused_prelight_map
-menu_focused_pressed = menu_focused_normal
-menu_focused_pressed_map = minimize_focused_prelight_map
-menu_unfocused = menu_focused_normal
-menu_unfocused_map = close_unfocused_map
+
+icons = {
+    # close
+    'close_focused_normal': IconMap(close_focused_normal, close_focused_normal_map),
+    'close_focused_prelight': IconMap(close_focused_normal, close_focused_prelight_map),
+    'close_focused_pressed': IconMap(close_focused_normal, close_focused_pressed_map),
+    'close_unfocused': IconMap(close_focused_normal, close_unfocused_map),
+
+    # hide
+    'minimize_focused_normal': IconMap(minimize_focused_normal, close_focused_normal_map),
+    'minimize_focused_prelight': IconMap(minimize_focused_normal, minimize_focused_prelight_map),
+    'minimize_focused_pressed': IconMap(minimize_focused_normal, minimize_focused_pressed_map),
+    'minimize_unfocused': IconMap(minimize_focused_normal, close_unfocused_map),
+
+    # maximize
+    'maximize_focused_normal': IconMap(maximize_focused_normal, close_focused_normal_map),
+    'maximize_focused_prelight': IconMap(maximize_focused_normal, minimize_focused_prelight_map),
+    'maximize_focused_pressed': IconMap(maximize_focused_normal, minimize_focused_pressed_map),
+    'maximize_unfocused': IconMap(maximize_focused_normal, close_unfocused_map),
+
+    # maximize-toggled
+    'unmaximize_focused_normal': IconMap(unmaximize_focused_normal, close_focused_normal_map),
+    'unmaximize_focused_prelight': IconMap(unmaximize_focused_normal, minimize_focused_prelight_map),
+    'unmaximize_focused_pressed': IconMap(unmaximize_focused_normal, minimize_focused_pressed_map),
+    'unmaximize_unfocused': IconMap(unmaximize_focused_normal, close_unfocused_map),
+
+    # menu
+    'menu_focused_normal': IconMap(menu_focused_normal, close_focused_normal_map),
+    'menu_focused_prelight': IconMap(menu_focused_normal, minimize_focused_prelight_map),
+    'menu_focused_pressed': IconMap(menu_focused_normal, minimize_focused_prelight_map),
+    'menu_unfocused': IconMap(menu_focused_normal, close_unfocused_map),
+}
 
 if __name__ == "__main__":
-    build()
+    build_unity(icons)
