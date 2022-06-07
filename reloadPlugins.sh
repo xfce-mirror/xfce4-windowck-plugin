11fb5e96a384fc131410444d89d3a0ab78e46f97
Arkadiy Illarionov <qarkai@gmail.com>
2022-06-07 21:15:58 +0300

    Simplify reloadPlugins.sh

--- reloadPlugins.sh
+++ reloadPlugins.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-for i in windowck wckbuttons
-    do
-    process=$(ps -ef | grep ${i} | grep -v grep | awk '{ print $2}')
-
-    if [[ -n ${process} ]]
-        then
-        kill ${process}
-    fi
+for i in windowck wckbuttons; do
+    pkill --full "lib${i}.so"
 done
