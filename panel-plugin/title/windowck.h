1c99a0cd6e036d62d05ad4dc78b7ee91b380a6dc
Akbarkhon Variskhanov <akbarkhon.variskhanov@gmail.com>
2022-06-10 19:45:28 +0500

    title: change size mode FIXE to FIXED

--- panel-plugin/title/windowck.h
+++ panel-plugin/title/windowck.h
@@ -45,7 +45,7 @@ typedef enum Alignment
 
 typedef enum SizeMode
 {
-    SHRINK = 1, FIXE = 2, EXPAND = 3
+    SHRINK = 1, FIXED = 2, EXPAND = 3
 } SizeMode;
 
 typedef struct {
@@ -64,7 +64,7 @@ typedef struct {
     gboolean two_lines;             // [T/F] Display the title on two lines
     gboolean show_tooltips;         // [T/F] Show tooltips
 
-    SizeMode size_mode;             // Size mode : Length=[MINIMAL,FIXE,EXPAND]
+    SizeMode size_mode;             // Size mode : Length=[MINIMAL,FIXED,EXPAND]
 
     gint title_size;                // Title size in chars
     gint title_padding;             // Title padding
