20cab09aed0b11887df15fe0a25282bafdff8704
Mubashshir <ahmubashshir@gmail.com>
2026-07-24 16:41:04 +0600

    spacer: Add spacer plugin
    Closes #46
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

---
+++ panel-plugin/spacer/wckspacer-spacer.h
@@ -0,0 +1,31 @@
+/*  $Id$
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  Copyright (C) 2013 Cedric Leporcq  <cedl38@gmail.com>
+ *                2026 Mubashshir      <ahmubashshir@gmail.com>
+ *
+ */
+
+#ifndef __WCKSPACER_SPACER_H__
+#define __WCKSPACER_SPACER_H__
+
+#include "wckspacer.h"
+
+void reload_wnck_spacer (WckSpacerPlugin *wsp);
+gboolean on_spacer_released(GtkWidget *icon, GdkEventButton *event, WckSpacerPlugin *wsp);
+gboolean on_spacer_pressed (GtkWidget *icon, GdkEventButton *event, WckSpacerPlugin *wsp);
+
+#endif /* __WCKSPACER_SPACER_H__ */
